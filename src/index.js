c5bfc586f646c4b727181d8fe5da5fe34d387a10
Travis Flak <travisflak@traviss-MacBook-Air.local>
2020-10-31 12:50:22 -0500

    defined reducers in index.js

--- src/index.js
+++ src/index.js
@@ -7,6 +7,36 @@ import registerServiceWorker from './registerServiceWorker';
 import { createStore, combineReducers } from 'redux';
 import { Provider } from 'react-redux';
 
+const feelingReducer = (state = [], action) => {
+
+    if(action.type === 'SET_FEELING') {
+        return action.payload;
+    } 
+    else if(action.type === 'SET_FEELING'){
+        return [...state, action.payload]
+    }
+    return state;
+}
+const understandingReducer = (state = [], action) => {
+    if (action.type === 'ADD_UNDERSTANDING'){
+        return [...state, action.payload]
+    }
+    return state;
+}
+
+const supportReducer = (state = [], action) => {
+    if (action.type === 'ADD_SUPPORT'){
+        return [...state, action.payload]
+    }
+    return state;
+}
+
+const commentsReducer = (state = [], action) => {
+    if (action.type === 'ADD_COMMENTS'){
+        return [...state, action.payload]
+    }
+    return state;
+}
 
 
 const storeInstance = createStore(
