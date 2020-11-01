1f4be37303b63bdcae804731d91846b3d43b7220
Travis Flak <travisflak@traviss-MacBook-Air.local>
2020-10-31 20:52:41 -0500

    I have the Feeling and Understanding components pathed correctly and they're rendered to the DOM

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,6 +4,7 @@ import axios from 'axios';
 import { HashRouter as Router, Route, Link, withRouter } from 'react-router-dom';
 import './App.css';
 import Feeling from '../Feeling/Feeling'
+import Understanding from '../Understanding/Understanding';
 
 
 class App extends Component {
@@ -28,17 +29,20 @@ class App extends Component {
 
   render() {
     return (
+      <Router>
       <div className="App">
         <header className="App-header">
           <h1 className="App-title">Feedback!</h1>
           <h4><i>Don't forget it!</i></h4>
         </header>
-          <Router>
+        
             {/* <Route exact path="/" component={Home}/> */}
             <Route path="/feeling" component={Feeling}/>
-          </Router>
+            <Route path="/understanding" component={Understanding}/>
+          
         <br/>
       </div>
+      </Router>
     );
   }
 }
